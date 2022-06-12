15ee7468c635a3cb11cf32273ae949ff81c280da
DeepaGheewala <deepaparikh18@gmail.com>
2022-06-12 12:57:31 -0500

    D2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,3 +1,4 @@
+# Deliverable 1: Linear Regression to Predict MPG
 # Use the library() function to load the dplyr package
 library(dplyr)
 library(tidyverse)
@@ -13,3 +14,17 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 # The P values is nearly 0 so thee is significance that we reject the null Hypothisis.
 # R-squared is o.71  which means the variables are strongly correlated 
+
+
+# Deliverable 2: Create Visualizations for the Trip Analysis 
+
+#Read the suspension coil data
+
+suspensionCoil_table <- read.csv('Suspension_Coil.csv',check.names = F, stringsAsFactors = F)
+
+# Generate Coil data summary
+total_summary = summarize(suspensionCoil_table, mean(PSI),median(PSI),var(PSI),sd(PSI))
+
+#Generate Manufacturing Lot wise summary
+lot_summary <- suspensionCoil_table %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI),median(PSI),var(PSI),sd(PSI))
+
